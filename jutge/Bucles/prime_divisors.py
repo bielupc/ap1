047341edffacc3022498a478316d2bbc39dbc83d
Biel Altimira <biel.altimira@estudiantat.upc.edu>
2022-10-02 12:31:40 +0200

    2-10-22 bucles jutge acabat

--- jutge/Bucles/prime_divisors.py
+++ jutge/Bucles/prime_divisors.py
@@ -2,16 +2,15 @@ from yogi import read
 
 n = read(int)
 
-d = 1
+i = 2
 
+while i <= n:
+    if n%i == 0:
+        print(i)
+        n //= i
+    else:
+        i += 1
 
-while d*d != 0:
-    if n % d == 0:
-        print(d, n//d)
-    d += 1
-
-if d * d == n:
-    print(d)
 
 
 
