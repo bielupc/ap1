9be1c68ca225f1be4525d6d0dc2407864d84e447
Biel Altimira <biel.altimira@estudiantat.upc.edu>
2022-10-19 09:01:07 +0200

    17-10-22 comparar cpp python

---
+++ 17-10-22/comparar.py
@@ -0,0 +1,30 @@
+print(x)    cout << x << endl;
+
+print(a, b)   cout << a << " " << b << end
+
+x = read(int)   int x; 
+                cin >> x;
+
+x = scan(int)      while (a >> x) {
+                    }
+
+for x in tokens(int):
+
+
+for i in range(n):
+    instr
+
+i = 0
+while i < n:
+    instr 
+    i += 1
+
+for (init; cond; pas){
+        instr
+        }
+
+init;
+while (cond){
+        instr;
+        pass;
+        }
