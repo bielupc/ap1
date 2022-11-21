6fbda9fcfdc14aa489d3efead642f8ec0de8ec8a
Biel Altimira <biel.altimira@estudiantat.upc.edu>
2022-11-21 10:08:02 +0100

    deadine

---
+++ fractathon/index.py
@@ -0,0 +1,25 @@
+import turtle as tl
+from math import sin
+
+
+
+
+
+
+def main() -> None:
+
+    # Ajustaments d'alguns paràmetres del Turtle
+    tl.speed(1)
+    tl.hideturtle()
+    tl.screensize(800, 600)
+    tl.bgcolor("black")
+    tl.title("test")
+
+
+
+
+    
+    tl.done()
+
+if __name__ == "__main__":
+    main()
